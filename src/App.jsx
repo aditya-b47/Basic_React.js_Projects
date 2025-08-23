6f260c3bdd9aa9c3354f5ed118ba809b05d6184d
eddy699 <barryallenakaflash995@gmail.com>
2025-08-23 12:45:52 +0530

    message

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import './App.css'
 import Accordian from './components/Accordion/Index'
 import Index from './components/Accordion/Index'
 import Color from './components/Color_Generator/Color'
+import Star from './components/Star-Rating/Star'
 
 function App() {
 
 
   return (
     <>
-    <Accordian/>
-    <Color/>
+    {/* <Accordian/> */}
+    {/* <Color/> */}
+    <Star noOfStars={10}/>
     </>
   )
 }
